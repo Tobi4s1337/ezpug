60f2949c4647e38ca4326c0e464310827bf5a10e
Tobias Klaus <dfz_tobias@hotmail.de>
2022-09-12 02:25:46 +0200

    mostly finish teamspeak integration

--- server/app/controllers/teamSpeak/index.js
+++ server/app/controllers/teamSpeak/index.js
@@ -2,6 +2,16 @@ const EventEmitter = require('events').EventEmitter
 const User = require('../../models/user')
 const { TeamSpeak } = require('ts3-nodejs-library')
 
+const createMatchChannelTitle = (teamOneName, teamTwoName) => {
+  let title = teamOneName + ' vs. ' + teamTwoName
+
+  if (title.length < 40) {
+    return title
+  }
+
+  return teamOneName.substring(0, 17) + ' vs. ' + teamTwoName.substring(0, 17)
+}
+
 const getLinkedIds = () => {
   return new Promise((resolve, reject) => {
     User.find({}, (err, users) => {
@@ -48,6 +58,11 @@ class TeamSpeakHandler extends EventEmitter {
     this._clients = []
     this._interval = null
     this._firstTime = true
+    this._queueChannel = null
+    this._dropsChannel = null
+    this._mapPoolChannel = null
+    this._generalTalkCid = null
+    this._matchAreaCid = null
   }
 
   async init() {
@@ -71,6 +86,8 @@ class TeamSpeakHandler extends EventEmitter {
         this.getClients()
       }, 3000)
 
+      await this.createDefaultChannels()
+
       return this
     } catch (err) {
       console.log('Error when connecting to TeamSpeak Server:', err)
@@ -78,6 +95,168 @@ class TeamSpeakHandler extends EventEmitter {
       setTimeout(() => {
         this.init()
       }, 1000 * 20)
+    }
+  }
+
+  async createDefaultChannels() {
+    try {
+      const blackSpacerTwo = await this.createChannel({
+        title: '[*spacer#15]━',
+        noJoin: true,
+        cpid: 0
+      })
+
+      const channelOrder = blackSpacerTwo.propcache.channelOrder
+
+      await this.createChannel({
+        title: '[cspacer#14]  ',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#13]Ｔ Ａ Ｌ Ｋ － Ｃ Ｈ Ａ Ｎ Ｎ Ｅ Ｌ',
+        noJoin: false,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#12]◢◤━━━━━━━ ◢〓◣ ━━━━━━━◥◣',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#11]  ',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      const matchAreaText = await this.createChannel({
+        title: '[cspacer#10]Ｍ Ａ Ｔ Ｃ Ｈ － Ａ Ｒ Ｅ Ａ',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      this._matchAreaCid = matchAreaText.propcache.cid
+
+      await this.createChannel({
+        title: '[cspacer#9]◢◤━━━━━━━ ◢〓◣ ━━━━━━━◥◣',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#8]',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#7]◥◣━━━━━━━ ◥〓☰〓◤ ━━━━━━━◢◤',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      this._mapPoolChannel = await this.createChannel({
+        title: '[cspacer#6]Map-Pool: Default',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      this._dropsChannel = await this.createChannel({
+        title: '[cspacer#5]Drops: Aktiv ✔️',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      this._queueChannel = await this.createChannel({
+        title: '[cspacer#4][0] In der Warteschlange',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer1#3]Ｅ Ｚ Ｐ Ｕ Ｇ ．Ｃ Ｏ Ｍ',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#2]◢◤━━━━━━━ ◢〓☰〓◣ ━━━━━━━◥◣',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[cspacer#1]  ',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+
+      await this.createChannel({
+        title: '[*spacer#0]━',
+        noJoin: true,
+        cpid: 0,
+        channelOrder
+      })
+    } catch (err) {
+      console.log('Failed to create default channels', err)
+
+      // Channels already exist, now get each of them for future reference
+      if (err.id === '771') {
+        this.getDefaultChannels()
+      }
+    }
+  }
+
+  async getDefaultChannels() {
+    try {
+      const channels = await this._teamSpeak.channelList()
+
+      for (const channel of channels) {
+        const name = channel.propcache.channelName
+        if (name.includes('[cspacer#6]Map-Pool')) {
+          this._mapPoolChannel = channel
+        }
+
+        if (name.includes('[cspacer#5]Drops')) {
+          this._dropsChannel = channel
+        }
+
+        if (name.includes('[cspacer#4]')) {
+          this._queueChannel = channel
+        }
+      }
+
+      const generalTalkChannel = await this._teamSpeak.getChannelByName(
+        '[cspacer#13]Ｔ Ａ Ｌ Ｋ － Ｃ Ｈ Ａ Ｎ Ｎ Ｅ Ｌ'
+      )
+
+      this._generalTalkCid = generalTalkChannel.propcache.cid
+
+      const matchAreaText = await this._teamSpeak.getChannelByName(
+        '[cspacer#10]Ｍ Ａ Ｔ Ｃ Ｈ － Ａ Ｒ Ｅ Ａ'
+      )
+      this._matchAreaCid = matchAreaText.propcache.cid
+
+      console.log('Got existing channels instead')
+    } catch (err) {
+      console.log('Error getting default channels', err)
     }
   }
 
@@ -158,6 +337,126 @@ class TeamSpeakHandler extends EventEmitter {
     } catch (err) {
       console.log('Error sending message to user', err)
     }
+  }
+
+  async moveUserToChannel({ teamSpeakId, cid }) {
+    try {
+      await this._teamSpeak.clientMove(teamSpeakId, cid)
+    } catch (err) {
+      console.log('Error moving player', err)
+    }
+  }
+
+  async moveUsersToChannel({ teamSpeakIds, cid }) {
+    try {
+      for (const teamSpeakId of teamSpeakIds) {
+        await this.moveUserToChannel({ teamSpeakId, cid })
+      }
+    } catch (err) {
+      console.log('Error moving players', err)
+    }
+  }
+
+  createChannel({ title, description, noJoin, cpid, channelOrder }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let props = {
+          channel_flag_permanent: '1',
+          channel_codec_quality: 10,
+          channel_description: description ? description : 'EZPUG.COM',
+          cpid: cpid ? cpid : 0
+        }
+
+        if (channelOrder) {
+          props.channelOrder = channelOrder
+        }
+
+        const channel = await this._teamSpeak.channelCreate(title, props)
+
+        if (noJoin) {
+          await channel
+            .setPerm()
+            .perm('i_channel_needed_join_power')
+            .value(90)
+            .skip(false)
+            .negate(false)
+            .update()
+        }
+
+        resolve(channel)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  async removeChannel({ cid }) {
+    try {
+      await this._teamSpeak.channelDelete(cid, 1)
+    } catch (err) {
+      console.log('Error removing channel', err)
+    }
+  }
+
+  async editChannel({ cid, options }) {
+    try {
+      this._teamSpeak.editChannel({ cid, options })
+    } catch (err) {
+      console.log('Error editing channel', err)
+    }
+  }
+
+  createMatch({ teamOneName, teamTwoName }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const mainChannelTitle = createMatchChannelTitle(
+          teamOneName,
+          teamTwoName
+        )
+
+        const mainChannel = await this.createChannel({
+          title: mainChannelTitle,
+          noJoin: true,
+          cpid: this._matchAreaCid
+        })
+        const mainCid = mainChannel.propcache.cid
+
+        const statusChannel = await this.createChannel({
+          title: 'Status: Player-Veto',
+          noJoin: true,
+          cpid: mainCid
+        })
+        const statusCid = statusChannel.propcache.cid
+
+        const talkChannel = await this.createChannel({
+          title: 'Talk / Playerveto',
+          cpid: mainCid
+        })
+        const talkCid = talkChannel.propcache.cid
+
+        const teamOneChannel = await this.createChannel({
+          title: teamOneName.substring(0, 40),
+          cpid: mainCid
+        })
+        const teamOneCid = teamOneChannel.propcache.cid
+
+        const teamTwoChannel = await this.createChannel({
+          title: teamTwoName.substring(0, 40),
+          cpid: mainCid
+        })
+        const teamTwoCid = teamTwoChannel.propcache.cid
+
+        resolve({
+          mainCid,
+          talkCid,
+          statusCid,
+          teamOneCid,
+          teamTwoCid
+        })
+      } catch (err) {
+        reject('Failed to create match channels', err)
+      }
+    })
   }
 }
 
